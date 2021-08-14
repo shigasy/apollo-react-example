0b108b370bb831645fd8efc285be5fc22ab457cc
shigasy <kxx555u.u@gmail.com>
2021-08-14 23:21:24 +0900

    github graphqlに client作って認証情報headerに入れて、query作って叩いた

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import gql from 'graphql-tag'
+import ApolloClient from "apollo-boost"
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+      }
+    })
+  }
+})
+
+const query = gql`
+{
+  organization(login: "apollographql") {
+    repositories(first: 5) {
+      nodes {
+        id
+        name
+        url
+        viewerHasStarred
+        stargazers {
+          totalCount
+        }
+      }
+    }
+  }
+}
+`
+client.query({
+  query
+}).then((result) => console.log(result))
 
 ReactDOM.render(
   <React.StrictMode>
